8d40502289112f0d55a019c0f0b25cff4d15c630
Björn Allvin <bjorn@allvin.se>
2020-08-27 13:59:19 +0200

    rrr

--- express/server.js
+++ express/server.js
@@ -6,12 +6,17 @@ const app = express();
 const bodyParser = require("body-parser");
 
 var jsonServer = require("json-server");
-//app.use("/api", jsonServer.router(path.resolve(__dirname, "db.json")));
+app.use("/api", jsonServer.router(path.resolve(__dirname, "db.json")));
 
 const router = express.Router();
 router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<h1>Hello 2 from Express.js!</h1>");
+  res.end();
+});
+router.get("/public/styles.css", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/css" });
+  res.write("");
   res.end();
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
